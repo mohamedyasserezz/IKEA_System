e33bbdf641ba4851ab9c10e23545a883b39baae7
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2024-09-20 14:56:44 +0300

    Part 01 Department Repositotry

---
+++ LinkDev.IKEA.DAL/Persistance/Repositories/Departments/IDepartmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using LinkDev.IKEA.DAL.Models.Department;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistance.Repositories.Departments
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll(bool WithAsNoTracking = true);
+        Department? GetById(int id);
+        int Add(Department entity);
+        int Update(Department entity);
+        int Delete(Department entity);
+    }
+}
