66975b7dd5a7d6d7f25bac34932685b6f6667222
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2024-09-20 12:05:11 +0300

    Part 05 Department Module (Entities, Configrations) and DbContext

---
+++ LinkDev.IKEA.DAL/Persistance/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using LinkDev.IKEA.DAL.Models.Department;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistance.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base()
+        {
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = IKEA; Trusted_Connection = true; TrustServerCertificate = true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ LinkDev.IKEA.DAL/Persistance/Data/Configurations/Departments/DepartmentConfigurations.cs
@@ -0,0 +1,23 @@
+﻿using LinkDev.IKEA.DAL.Models.Department;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistance.Data.Configurations.Departments
+{
+    internal class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+            builder.Property(D => D.Name).HasColumnType("VarChar(50)").IsRequired();
+            builder.Property(D => D.Code).HasColumnType("VarChar(20)").IsRequired();
+            builder.Property(D => D.CreatedOn).HasDefaultValueSql("GetDate()");
+            builder.Property(D => D.LastModifiedOn).HasComputedColumnSql("GetDate()");
+        }
+    }
+}
