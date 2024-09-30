767fba8de4bb2ab3d63d4f472bb0a51f404da95b
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2024-09-30 13:55:58 +0300

    Part 02 Microsoft Identity Package

--- LinkDev.IKEA.DAL/Persistance/Data/ApplicationDbContext.cs
+++ LinkDev.IKEA.DAL/Persistance/Data/ApplicationDbContext.cs
@@ -1,29 +1,29 @@
-﻿using LinkDev.IKEA.DAL.Common.Enums;
-using LinkDev.IKEA.DAL.Entities.Departments;
+﻿using LinkDev.IKEA.DAL.Entities.Departments;
 using LinkDev.IKEA.DAL.Entities.Employees;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LinkDev.IKEA.DAL.Persistance.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
         }
-       
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
         }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
+       
+
     }
 }
