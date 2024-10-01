dfc21b3c3dfce131e35ebc3c67973c60a47fe2fe
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2024-10-01 20:21:01 +0300

    Part 04 Account Controller - SignIn

--- LinkDev.IKEA.PL/Controllers/AccountController.cs
+++ LinkDev.IKEA.PL/Controllers/AccountController.cs
@@ -2,59 +2,85 @@
 using LinkDev.IKEA.PL.ViewModels.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace LinkDev.IKEA.PL.Controllers
 {
-	public class AccountController(
-		UserManager<ApplicationUser> _userManager,
-		SignInManager<ApplicationUser> _signInManager) : Controller
-	{
-		#region Sign UP
-		[HttpGet]
-		public IActionResult SignUp()
-		{
-			return View();
-		}
-		[HttpPost]
-		public async Task<IActionResult> SignUp(SignUpViewModel model)
-		{
-			if (!ModelState.IsValid)
-				return BadRequest();
+    public class AccountController(
+        UserManager<ApplicationUser> _userManager,
+        SignInManager<ApplicationUser> _signInManager) : Controller
+    {
+        #region Sign UP
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
 
-			var user = await _userManager.FindByNameAsync(model.UserName);
-			if (user == null)
-			{
-				user = new ApplicationUser
-				{
-					FName = model.FirstName,
-					LName = model.LastName,
-					UserName = model.UserName,
-					Email = model.Email,
-					IsAgree = model.IsAgree,
-				};
-				var result = await _userManager.CreateAsync(user, model.Password);
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user == null)
+            {
+                user = new ApplicationUser
+                {
+                    FName = model.FirstName,
+                    LName = model.LastName,
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    IsAgree = model.IsAgree,
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
 
-				if (result.Succeeded)
-					RedirectToAction(nameof(SignIn));
+                if (result.Succeeded)
+                    RedirectToAction(nameof(SignIn));
 
-				foreach (var error in result.Errors)
-				{
-					ModelState.AddModelError(string.Empty, error.Description);
-				}
-			}
-			else
-				ModelState.AddModelError(nameof(SignUpViewModel.UserName), "This User Name is already exist");
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            else
+                ModelState.AddModelError(nameof(SignUpViewModel.UserName), "This User Name is already exist");
 
-			return View(model);
-		}
-		#endregion
+            return View(model);
+        }
+        #endregion
 
-		#region Sign In
-		[HttpGet]
-		public IActionResult SignIn()
-		{
-			return View();
-		}
-		#endregion
-	}
+        #region Sign In
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewModel model)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            
+            if (user is { }) 
+            {
+                var flag = await _userManager.CheckPasswordAsync(user, model.Password);
+                if (flag)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+                    if (result.IsNotAllowed)
+                        ModelState.AddModelError(string.Empty, "Your Account is not allowed!!");
+                    if (result.IsLockedOut)
+                        ModelState.AddModelError(string.Empty, "Your Account is Locked!!");
+                    if (result.Succeeded)
+                        return RedirectToAction("Index", "Home");
+                }
+               
+            }
+            ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            return View(model);
+        }
+        #endregion
+    }
 }
